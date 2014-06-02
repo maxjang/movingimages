b2be91d901456b3f1a4401f9fa6c038171217f42
Max Tiong <maxtiongoc@gmail.com>
2014-06-02 02:47:28 +0000

    added production api keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :instagram, 'c6dc5a77db6942f3a22c9e6a469972b2', '08d87c472e9b4f95988a04524580d0f1'
+  if (Rails.env == 'development' || Rails.env == 'test')
+    provider :instagram, 'c6dc5a77db6942f3a22c9e6a469972b2', '08d87c472e9b4f95988a04524580d0f1'
+  elsif(Rails.env == 'production')
+    provider :instagram, '6aa594edd5c042aeb6d5e940d8bcd85c', '5bb315185225491caf9ccbf0868befd0'
+  end
 end

1ba0270cb14004d1a5df01039a03d8aef098ad92
Max Tiong <maxtiongoc@gmail.com>
2014-06-04 04:31:16 +0000

    Added Map View and Index Pagination

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,6 +1,6 @@
 class ExperiencesController < ApplicationController
   require "instagram"
-
+  
   def index
     if session[:user_id] != nil
       user = User.find(session[:user_id])
@@ -8,8 +8,14 @@ class ExperiencesController < ApplicationController
         config.client_id = "c6dc5a77db6942f3a22c9e6a469972b2"
         config.access_token = user.token
       end
-      @experiences = Instagram.user_media_feed(user.uid)
+      max_id = params[:max_id] if params[:max_id]
+      @experiences = Instagram.tag_recent_media("food_korea", {max_tag_id: max_id})
+      @max_tag_id = Instagram.tag_recent_media("food_korea", {max_tag_id: max_id} ).pagination[:next_max_tag_id]
     end
+  end
+
+  def show
+    @experience = Instagram.media_item(params[:id])
   end
 
 end
